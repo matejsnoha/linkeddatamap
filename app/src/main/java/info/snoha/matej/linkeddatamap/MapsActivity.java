ee768a7ba7ab499daf2daecacd7ff0a88f465889
Matej Snoha <matej@snoha.info>
2016-01-06 01:31:38 +0100

    Centered map with floating action button for current location.

--- app/src/main/java/info/snoha/matej/linkeddatamap/MapsActivity.java
+++ app/src/main/java/info/snoha/matej/linkeddatamap/MapsActivity.java
@@ -1,46 +1,128 @@
 package info.snoha.matej.linkeddatamap;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Location;
+import android.os.Looper;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
+    private GoogleApiClient apiClient;
+
+    protected void onStart() {
+        super.onStart();
+        apiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (apiClient.isConnected())
+            apiClient.disconnect();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setIcon(R.mipmap.ic_launcher);
+        }
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Location location = getCurrentLocation();
+                if (location != null) {
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                            new LatLng(location.getLatitude(), location.getLongitude()), 16));
+                } else {
+                    Snackbar.make(getRootView(), "Location not available", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng mapCenter = new LatLng(50.0819015, 14.4326654);
+        //map.addMarker(new MarkerOptions().position(mapCenter).title("Prague"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 6));
+
+    }
+
+    private Location getCurrentLocation() {
+        try {
+            return LocationServices.FusedLocationApi.getLastLocation(apiClient);
+        } catch (SecurityException e) {
+            return null;
+        }
+    }
+
+    private View getRootView() {
+        return getWindow().getDecorView().getRootView();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Snackbar.make(getRootView(), "Google APIs connected", Snackbar.LENGTH_LONG).show();
+
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(5000);
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(apiClient, request, new LocationCallback() {
+
+                @Override
+                public void onLocationResult(LocationResult result) {
+                    super.onLocationResult(result);
+                }
+            }, Looper.getMainLooper());
+        } catch (SecurityException e) {
+            Snackbar.make(getRootView(), "Missing location permission", Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Snackbar.make(getRootView(), "Google APIs disconnected", Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Snackbar.make(getRootView(), "Google APIs failed to connect", Snackbar.LENGTH_LONG).show();
     }
 }
